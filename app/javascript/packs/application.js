44129230e44bf125a65b9c69a3c5ce0cc8446cc1
tadejdelopst <tadejdelopst@gmail.com>
2021-03-07 20:52:10 +0100

    neki od bootstrapa

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,33 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("bootstrap")
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+// CSS
+import 'scss/site'
+// JS
+import('js/site')
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+// Images
+const images = require.context('../images', true)
+const imagePath = (name) => images(name, true)
+
+// Stimulus
+/*
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
+
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
+*/
